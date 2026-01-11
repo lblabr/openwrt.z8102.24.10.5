f173e49ee5cf34e0dd4a802fba1d8927e7e2fde7
lblabr <lblabr@gmail.com>
2026-01-11 19:13:24 +0100

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git custom https://github.com/lblabr/openwrt.custom-packages.git' >>feeds.conf
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
